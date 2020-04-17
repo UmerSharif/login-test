3cb3df1bd7cb6ba15e2613d5db4f30bad3dc0aa5
umerkhan <umer4167@hotmail.com>
2020-04-17 14:15:29 +0300

    Add init

---
+++ src/GoogleLogin.js
@@ -0,0 +1,24 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import axios from "axios";
+
+const responseGoogle = async (response) => {
+  //   let res = await axios.post(
+  //     "http://localhost:3001/api/v1/users/google-authenticate",
+  //     { id_token: response.tokenObj.id_token }
+  //   );
+  console.log(response.tokenObj.id_token);
+  //console.log(res.data);
+};
+
+export default function GoogleSign() {
+  return (
+    <GoogleLogin
+      clientId="1060877864557-efi4ju1lkp8bpbpb14grt1o3lj4t1v1u.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+    />
+  );
+}
